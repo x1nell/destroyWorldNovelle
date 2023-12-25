b55b79cf698c7c6622e1566a167d52650ee8d9b1
roman <123414028+x1nell@users.noreply.github.com>
2023-12-25 03:26:13 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const storyParts = [
-  { text: "Текст части 1", imageUrl: "URL_Картинки_1" },
-  { text: "Текст части 2", imageUrl: "URL_Картинки_2" },
-  { text: "Текст части 3", imageUrl: "URL_Картинки_3" }
+  { text: "Текст части 1", imageUrl: "img/1a.jpg" },
+  { text: "Текст части 2", imageUrl: "img/1a.jpg" },
+  { text: "Текст части 3", imageUrl: "img/1a.jpg" }
   // Добавьте другие части и изображения по аналогии
 ];
 
@@ -24,19 +24,99 @@ function nextPart() {
   localStorage.setItem("currentPart", currentPart);
 }
 
+function showMenu() {
+  const menuContainer = document.getElementById('menuContainer');
+  const menuList = document.getElementById('menuList');
+  
+  // Очищаем текущее меню
+  menuList.innerHTML = "";
+  
+  // Создаем элементы меню для каждой части
+  for (let i = 0; i < storyParts.length; i++) {
+    const menuItem = document.createElement('li');
+    menuItem.textContent = `Глава ${i + 1}`;
+    menuItem.onclick = () => goToPart(i);
+    menuList.appendChild(menuItem);
+  }
+  
+  // Показываем меню
+  menuContainer.style.display = "block";
+}
+
+function goToPart(partIndex) {
+  displayStoryPart(partIndex);
+  localStorage.setItem("currentPart", partIndex);
+  closeMenu();
+}
+
+const MAX_SAVED_SLOTS = 3; // Максимальное количество сохраненных ячеек
+
+let savedProgress = JSON.parse(localStorage.getItem("savedProgress")) || Array(MAX_SAVED_SLOTS).fill(null);
+
+function openMenu() {
+  showMenu();
+  updateMenuList();
+  document.getElementById('menuContainer').style.display = "block";
+}
+
+function closeMenu() {
+  document.getElementById('menuContainer').style.display = "none";
+}
+
 function saveProgress() {
-  localStorage.setItem("savedPart", currentPart);
-  alert("Прогресс сохранен!");
+  const currentSlot = prompt("Введите номер ячейки сохранения (1-" + MAX_SAVED_SLOTS + "):");
+  const slotIndex = parseInt(currentSlot) - 1;
+
+  if (!isNaN(slotIndex) && slotIndex >= 0 && slotIndex < MAX_SAVED_SLOTS) {
+    savedProgress[slotIndex] = currentPart;
+    localStorage.setItem("savedProgress", JSON.stringify(savedProgress));
+    updateMenuList();
+    alert("Прогресс сохранен в ячейке " + (slotIndex + 1) + "!");
+  } else {
+    alert("Неправильный номер ячейки сохранения!");
+  }
 }
 
 function loadProgress() {
-  const savedPart = parseInt(localStorage.getItem("savedPart"));
-  if (!isNaN(savedPart) && savedPart >= 0 && savedPart < storyParts.length) {
-    currentPart = savedPart;
-    displayStoryPart(currentPart);
-    localStorage.setItem("currentPart", currentPart);
-    alert("Прогресс загружен!");
+  const selectedSlot = prompt("Введите номер ячейки сохранения (1-" + MAX_SAVED_SLOTS + "):");
+  const slotIndex = parseInt(selectedSlot) - 1;
+
+  if (!isNaN(slotIndex) && slotIndex >= 0 && slotIndex < MAX_SAVED_SLOTS) {
+    if (savedProgress[slotIndex] !== null) {
+      currentPart = savedProgress[slotIndex];
+      displayStoryPart(currentPart);
+      localStorage.setItem("currentPart", currentPart);
+      closeMenu(); // Закрываем меню после загрузки прогресса
+      alert("Прогресс загружен из ячейки " + (slotIndex + 1) + "!");
+    } else {
+      alert("В этой ячейке нет сохраненного прогресса!");
+    }
   } else {
-    alert("Нет сохраненного прогресса!");
+    alert("Неправильный номер ячейки сохранения!");
+  }
+}
+
+function updateMenuList() {
+  const menuList = document.getElementById('menuList');
+  menuList.innerHTML = "";
+  
+  for (let i = 0; i < storyParts.length; i++) {
+    const listItem = document.createElement('li');
+    listItem.textContent = "Глава " + (i + 1) + ": " + storyParts[i].text;
+    listItem.onclick = function() {
+      currentPart = i;
+      displayStoryPart(currentPart);
+      closeMenu();
+    };
+    menuList.appendChild(listItem);
+  }
+
+  for (let i = 0; i < MAX_SAVED_SLOTS; i++) {
+    const listItem = document.createElement('li');
+    listItem.textContent = "Ячейка " + (i + 1) + ": " + (savedProgress[i] !== null ? "Прогресс сохранен" : "Пусто");
+    listItem.onclick = function() {
+      loadProgress(i);
+    };
+    menuList.appendChild(listItem);
   }
 }
